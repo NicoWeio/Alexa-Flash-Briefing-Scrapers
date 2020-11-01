b4c9ea84895afb64978097915ced0daa472dbf1f
Nicolai Weitkemper <nico.weio@gmail.com>
2020-11-01 21:52:15 +0100

    speed up scraping by parallelizing article loads

--- golem.js
+++ golem.js
@@ -7,41 +7,15 @@ async function request(url) {
     headers: {
       'Cookie': 'golem_consent20=cmp|200801',
     },
+    timeout: 10000,
   })).data;
 }
 
 async function get() {
   let html = await request(URL);
-  let items = [];
-
   let elements = $('.list-articles li', html);
-
-  for (let i = 0; i < 5; i++) {
-    let e = elements[i];
-
-    let heading = $(e).find('header > a > div > hgroup >.head2').text();
-
-    let contentEl = $(e).find('p').first();
-    contentEl.find('em').remove();
-    let content = contentEl.text()
-      .replace("Hinweis: Um sich diesen Artikel vorlesen zu lassen, klicken Sie auf den Player im Artikel.", '')
-      .replace(/\s\s+/g, ' ')
-      .trim();
-
-    let href = $(e).find('a')[0].attribs.href;
-
-    // visit article to check date
-    let articleHtml = await request(href);
-    // full path: '#screen > div:nth-child(2) > article > header > div.authors.authors--withsource > time'
-    let datetime = $('time', articleHtml).attr('datetime');
-
-    items.push({
-      heading,
-      content,
-      href,
-      datetime,
-    });
-  }
+  let elementsToScrape = elements.slice(0, 5).get();
+  let items = await Promise.all(elementsToScrape.map(async e => getSingle(e)));
 
   let feedItems = items
     .map(item => {
@@ -56,6 +30,31 @@ async function get() {
     .filter(feedItem => !["Sonst noch was?"].includes(feedItem.titleText));
 
   return feedItems;
+}
+
+async function getSingle(e) {
+  let heading = $(e).find('header > a > div > hgroup >.head2').text();
+
+  let contentEl = $(e).find('p').first();
+  contentEl.find('em').remove();
+  let content = contentEl.text()
+    .replace("Hinweis: Um sich diesen Artikel vorlesen zu lassen, klicken Sie auf den Player im Artikel.", '')
+    .replace(/\s\s+/g, ' ')
+    .trim();
+
+  let href = $(e).find('a')[0].attribs.href;
+
+  // visit article to check date
+  let articleHtml = await request(href);
+  // full path: '#screen > div:nth-child(2) > article > header > div.authors.authors--withsource > time'
+  let datetime = $('time', articleHtml).attr('datetime');
+
+  return {
+    heading,
+    content,
+    href,
+    datetime,
+  };
 }
 
 module.exports = get;
